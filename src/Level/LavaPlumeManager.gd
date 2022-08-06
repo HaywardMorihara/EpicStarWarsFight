288dde6ca8503f289533edcffddb691dcc96df04
HaywardMorihara <nathanielmorihara@gmail.com>
2022-08-06 16:51:22 -0400

    A lot done

---
+++ src/Level/LavaPlumeManager.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var limit_left : float
+var limit_right : float
+
+var lava_plumes : Array
+var next_lava_plume_index := 0
+
+func _ready():
+	for child in get_children():
+		if child.is_in_group("lava_plumes"):
+			lava_plumes.append(child)
+
+func _on_Timer_timeout():
+	var lava_plume = lava_plumes[next_lava_plume_index]
+	
+	if next_lava_plume_index == len(lava_plumes) - 1:
+		next_lava_plume_index = 0
+	else:
+		next_lava_plume_index += 1
+	
+	var randx = rand_range(limit_left, limit_right)
+	var scale = rand_range(0.5, 2.0)
+	print(scale)
+	lava_plume.scale = Vector2(scale, scale)
+	lava_plume.position = Vector2(randx, lava_plume.position.y)
+	lava_plume.restart()
--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-const LIMIT_LEFT = -315
-const LIMIT_TOP = -250
-const LIMIT_RIGHT = 955
-const LIMIT_BOTTOM = 690
-
 func _ready():
+	var width = $Bounds/CollisionShape2D.shape.extents.x
+	var x = $Bounds.get_position().x
 	for child in get_children():
 		if child is Player:
 			var camera = child.get_node("Camera")
-			camera.limit_left = LIMIT_LEFT
-			camera.limit_top = LIMIT_TOP
-			camera.limit_right = LIMIT_RIGHT
-			camera.limit_bottom = LIMIT_BOTTOM
+			camera.limit_left = x - width
+			camera.limit_right = x + width
+#			camera.limit_top = LIMIT_TOP
+#			camera.limit_bottom = LIMIT_BOTTOM
+		if child.is_in_group("lava_plume_managers"):
+			child.limit_left = x - width
+			child.limit_right = x + width
